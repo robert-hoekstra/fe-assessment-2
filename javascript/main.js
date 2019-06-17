de97a5d63931286922fcfe214731e9a628099830
Robert Hoekstra <robert.hoekstra@hva.nl>
2019-06-17 12:18:06 +0200

    update javascript and css

--- javascript/main.js
+++ javascript/main.js
@@ -1,17 +1,28 @@
-// When the user scrolls the page, execute myFunction 
-window.onscroll = function() {myFunction()};
+// When the user scrolls the page, execute activateProgressbar(); 
+// Scroll function is copied. However the progressive enhancement has been written by me (disabling and enabling the feauture.)
+window.onscroll = function() {activateProgressbar()};
 
-function myFunction() {
+function activateProgressbar() {
   var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
   var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
   var scrolled = (winScroll / height) * 100;
   document.getElementById("myBar").style.width = scrolled + "%";
 }
+// Fire function when page has loaded
+document.onload = deployProgressbar();
+
+// remove class and add new one
+function deployProgressbar() {
+    console.log('page loaded');
+    style = document.getElementsByClassName('progress-container-hidden')
+    style[0].classList.add('progress-container-shown');
+    style[0].classList.remove('progress-container-hidden');
+
+}
+
+
 
 // Code from:  https://www.w3schools.com/howto/howto_js_scroll_indicator.asp
-
-
-
 
 
 // Writing the function like this results in JavaScript crashing when there is no node to be found. 
@@ -29,16 +40,14 @@ if (lever) {
 document.querySelector('#styleMain').onclick = function () { changeCSS("../css/mainStyle.css") };
 document.querySelector('#styleAccessibility').onclick = function () { changeCSS("../css/accessibilityStyle.css"); };
 
+// Sets stylesheet reference to other reference and ignores native behaviour.
 function changeCSS(cssFile) {
     document.getElementById("cssID").setAttribute("href", cssFile);
     this.event.returnValue = false; 
 }
 
-
+// Toggle between classes to hide and show list items.
 function classToggle() {
     this.classList.toggle('hidden');
     this.classList.toggle('summary');
 }
-
-
-  
